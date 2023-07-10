95168890e316e5ebba45440abc70ada494724065
NicholasPilotto <nicholaspilotto@gmail.com>
2023-07-10 21:14:20 +0200

    First project commit

---
+++ CryptoTracker/CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Nicholas Pilotto on 10/07/23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
