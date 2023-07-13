787c38a19609adb310e98dbba80fe4ce7e1d921c
NicholasPilotto <nicholaspilotto@gmail.com>
2023-07-13 22:27:10 +0200

    Adding header to home view

--- CryptoTracker/CryptoTracker/Core/Home/Views/HomeView.swift
+++ CryptoTracker/CryptoTracker/Core/Home/Views/HomeView.swift
@@ -8,13 +8,61 @@
 import SwiftUI
 
 struct HomeView: View {
+  @State private var showPortfolio: Bool = false
+  
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      ZStack {
+        Color.theme.background
+          .ignoresSafeArea()
+        
+        VStack {
+          homeHeader
+          Spacer(minLength: 0)
+        }
+      }
     }
+}
+
+extension HomeView {
+  private var homeHeader: some View {
+    HStack {
+      CircleButtonView(iconeName: showPortfolio ? "plus" : "info")
+        .transaction { transaction in
+          transaction.animation = nil
+        }
+        .background {
+          CircleButtonAnimationView(animate: $showPortfolio)
+        }
+        
+      Spacer()
+      
+      Text(showPortfolio ? "Portfolio" : "Live prices")
+        .font(.headline)
+        .fontWeight(.heavy)
+        .foregroundColor(Color.theme.accent)
+        .transaction { transaction in
+            transaction.animation = nil
+        }
+      
+      Spacer()
+      
+      CircleButtonView(iconeName: "chevron.right")
+        .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
+        .onTapGesture {
+          withAnimation(.spring()) {
+            showPortfolio.toggle()
+          }
+        }
+    }
+    .padding(.horizontal)
+  }
 }
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
+      NavigationView {
         HomeView()
+          .navigationBarHidden(true)
+      }
     }
 }
