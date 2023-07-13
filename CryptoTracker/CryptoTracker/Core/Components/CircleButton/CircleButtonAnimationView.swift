3f320089e37ec2ac4045365aaf6b940d8881362f
NicholasPilotto <nicholaspilotto@gmail.com>
2023-07-13 22:26:44 +0200

    Animation for circle button

--- CryptoTracker/CryptoTracker/Core/Components/CircleButton/CircleButtonAnimationView.swift
+++ CryptoTracker/CryptoTracker/Core/Components/CircleButton/CircleButtonAnimationView.swift
@@ -8,13 +8,21 @@
 import SwiftUI
 
 struct CircleButtonAnimationView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+  @Binding var animate: Bool
+  var body: some View {
+    Circle()
+      .stroke(lineWidth: 5.0)
+      .scale(animate ? 1.0 : 0.0)
+      .opacity(animate ? 0.0 : 1.0)
+      .animation(animate ? Animation.easeOut(duration: 1.0) : .none, value: animate)
+    
+  }
 }
 
 struct CircleButtonAnimationView_Previews: PreviewProvider {
-    static var previews: some View {
-        CircleButtonAnimationView()
-    }
+  static var previews: some View {
+    CircleButtonAnimationView(animate: .constant(false))
+      .foregroundColor(.red)
+      .frame(width: 100, height: 100)
+  }
 }
