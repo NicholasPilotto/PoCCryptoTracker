9deb0799eaddf3ee5d2de62727dc4213c0af6cef
NicholasPilotto <nicholaspilotto@gmail.com>
2023-07-18 21:59:10 +0200

    Coin image component

---
+++ CryptoTracker/CryptoTracker/Core/Components/CoinImage/CoinImageViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CoinImageViewModel.swift
+//  CryptoTracker
+//
+//  Created by Nicholas Pilotto on 18/07/23.
+//
+
+import Foundation
---
+++ CryptoTracker/CryptoTracker/Services/CoinImageService.swift
@@ -0,0 +1,8 @@
+//
+//  CoinImageService.swift
+//  CryptoTracker
+//
+//  Created by Nicholas Pilotto on 18/07/23.
+//
+
+import Foundation
