d80bd8bfda2a6414cbc7ed0d6fa7c2c07baa2b58
NicholasPilotto <nicholaspilotto@gmail.com>
2023-07-21 22:39:33 +0200

    Filter coins array on search

--- CryptoTracker/CryptoTracker/Core/Home/ViewModels/HomeViewModel.swift
+++ CryptoTracker/CryptoTracker/Core/Home/ViewModels/HomeViewModel.swift
@@ -9,17 +9,33 @@ class HomeViewModel: ObservableObject {
   @Published var allCoins: [CoinModel] = []
   @Published var portfolioCoins: [CoinModel] = []
   @Published var searchedText: String = ""
-
+  
   private let dataService = CoinDataService()
   private var cancellables = Set<AnyCancellable>()
-
+  
   init() {
     addSubscribers()
   }
-
-  func addSubscribers() {
-    dataService.$allCoins
-      .sink { [weak self] returnedCoins in
+  
+  private func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
+    guard !text.isEmpty else {
+      return coins
+    }
+    
+    let lowercaseText = text.lowercased()
+    return coins.filter { (coin) in
+      return coin.name.lowercased().contains(lowercaseText) ||
+      coin.symbol.lowercased().contains(lowercaseText) ||
+      coin.id.lowercased().contains(lowercaseText)
+    }
+  }
+  
+  func addSubscribers() {    
+    $searchedText
+      .combineLatest(dataService.$allCoins)
+      .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+      .map(filterCoins)
+      .sink { [weak self] (returnedCoins) in
         self?.allCoins = returnedCoins
       }
       .store(in: &cancellables)
