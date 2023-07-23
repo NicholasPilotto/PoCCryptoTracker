01db5f129ebb81db8bb6b8e52f88b7e227787736
NicholasPilotto <nicholaspilotto@gmail.com>
2023-07-23 12:45:12 +0200

    Get market data

--- CryptoTracker/CryptoTracker/Core/Home/ViewModels/HomeViewModel.swift
+++ CryptoTracker/CryptoTracker/Core/Home/ViewModels/HomeViewModel.swift
@@ -17,7 +17,8 @@ class HomeViewModel: ObservableObject {
   @Published var portfolioCoins: [CoinModel] = []
   @Published var searchedText: String = ""
 
-  private let dataService = CoinDataService()
+  private let coinDataService = CoinDataService()
+  private let markedDataService = MarketDataService()
   private var cancellables = Set<AnyCancellable>()
 
   init() {
@@ -37,14 +38,40 @@ class HomeViewModel: ObservableObject {
     }
   }
 
+  private func mapGlobalMarketData(markedDataModel: MarketDataModel?) -> [StatisticModel] {
+    var stats: [StatisticModel] = []
+    guard let data = markedDataModel else {
+      return stats
+    }
+    
+    stats.append(contentsOf: [
+      StatisticModel(title: "Market cap", value: data.marketCap, percentageChange: data.marketCapChangePercentage24HUsd),
+      StatisticModel(title: "Volume", value: data.volume),
+      StatisticModel(title: "BTC dominance", value: data.btcDominance),
+      StatisticModel(title: "Portfolio value", value: "$0.00", percentageChange: 0)
+    ])
+    
+    return stats
+  }
+  
   func addSubscribers() {
+    // update coins data
     $searchedText
-      .combineLatest(dataService.$allCoins)
+      .combineLatest(coinDataService.$allCoins)
       .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
       .map(filterCoins)
       .sink { [weak self] returnedCoins in
         self?.allCoins = returnedCoins
       }
       .store(in: &cancellables)
+    
+    // updates market data
+    markedDataService.$marketData
+      .map(mapGlobalMarketData)
+      .sink { [weak self] (marketDataReturned) in
+        self?.statistics = marketDataReturned
+      }
+      .store(in: &cancellables)
   }
+  
 }
