3f47547e90694b7891d70bae1e92144dd6c657ba
NicholasPilotto <nicholaspilotto@gmail.com>
2023-07-23 12:44:54 +0200

    Market data service class

--- CryptoTracker/CryptoTracker/Services/MarketDataService.swift
+++ CryptoTracker/CryptoTracker/Services/MarketDataService.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+import Combine
+
+class MarketDataService {
+  @Published var marketData: MarketDataModel? = nil
+
+  var marketDataSubscription: AnyCancellable?
+
+  init() {
+    getData()
+  }
+
+  private func getData() {
+    guard let url = URL(string: "https://api.coingecko.com/api/v3/global") else {
+      return
+    }
+
+    let decoder = JSONDecoder()
+    decoder.keyDecodingStrategy = .convertFromSnakeCase
+
+    marketDataSubscription = NetworkingManager.download(url: url)
+      .decode(type: GlobalData.self, decoder: decoder)
+      .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] receivedData in
+        
+        self?.marketData = receivedData.data
+        self?.marketDataSubscription?.cancel()
+      })
+  }
+}
